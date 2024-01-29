aea62bb3d4fedc7f9abd5ba0665de1ccc0cef886
Fade <64497456+FadeRemix@users.noreply.github.com>
2024-01-29 10:12:04 -0500

    Delete assets directory

--- assets/js/scrolling.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var enterText = document.getElementById('enter-text');
-    var mainContent = document.getElementById('main-content');
-
-    enterText.addEventListener('click', function () {
-        mainContent.style.display = 'block';
-        enterText.style.display = 'none'; // Hide the "Click to Enter" text
-    });
-});
