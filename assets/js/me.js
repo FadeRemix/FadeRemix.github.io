2b0eabd4f58bc2bb1c0ed4e90624772998973d2c
Fade <64497456+FadeRemix@users.noreply.github.com>
2024-01-29 10:28:49 -0500

    Add files via upload

--- assets/js/me.js
+++ assets/js/me.js
@@ -1,1 +1,25 @@
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
 
+function loaded(){
+
+	setInterval(loop, 350);
+
+}
+
+var x = 0;
+
+var titleText = [ "→ fade.rip","⇉ fade.rip","⇶ fade.rip"];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
