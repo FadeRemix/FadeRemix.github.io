d168e0d140afc9df2a0c19e2f5ff82de1e2268f1
fade.rip <64497456+FadeRemix@users.noreply.github.com>
2024-08-21 13:25:36 -0400

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -2,20 +2,20 @@ document.getElementById('enter-text').addEventListener('click', function() {
 const videoMap = new Map();
 const scrolltextMap = new Map();
 
-videoMap.set(0,"https://cdn.discordapp.com/attachments/1224928873230368918/1224933952339443752/boogie.mp4?ex=661f4ba4&is=660cd6a4&hm=ae8416a8b0d0d811118972ef421978bf55b58a01048e0a9e2a33f092e20b4619&") // Video 1
-scrolltextMap.set(0,"Boogie - Still Think About You");
+videoMap.set(0,"https://cdn.discordapp.com/attachments/1224928873230368918/1275867705311756368/YNW_Melly_YNW_BSlime__Ynw4L-_772_Love_Pt.3_Your_Love_Music_Video_ynwmelly_ynwbslime_yourlove.mp4?ex=66c773e5&is=66c62265&hm=db57250f7addd1bc0cc7872f440189df41ceae4e1e9837882725417f05ae99ad&") // Video 1
+scrolltextMap.set(0,"YNW Melly - 772 Love Pt3");
 
-videoMap.set(1,"https://cdn.discordapp.com/attachments/1224928873230368918/1224933956298866699/draft.mp4?ex=661f4ba5&is=660cd6a5&hm=b04e67b8523616bb5f364c126982504f67ad4cc6db7637b574c64dd1319c3103&") // Video 2
-scrolltextMap.set(1,"Draft Day - LUV DRUGS");
+videoMap.set(1,"https://cdn.discordapp.com/attachments/1224928873230368918/1275867703172661299/Lil_Yachty__Veeze_-_Sorry_Not_Sorry_Official_Music_Video.mp4?ex=66c773e4&is=66c62264&hm=6e932197e4421d35f1fbea52f21800399e79057c4f2be7dc25174130a63ea584&") // Video 2
+scrolltextMap.set(1,"Veeze - Sorry Not Sorry");
 
-videoMap.set(2,"https://cdn.discordapp.com/attachments/1224928873230368918/1224933957607493696/gherbo.mp4?ex=661f4ba5&is=660cd6a5&hm=0b3fa829e9aacfaad1d95f60e0dff8cd5b8751f879d4ed833001eedf24c30e20&") // Video 3
-scrolltextMap.set(2,"G Herbo - Sessions");
+videoMap.set(2,"https://cdn.discordapp.com/attachments/1224928873230368918/1275867708914663537/Luh_Tyler_-_Young_N_____Official_Music_Video.mp4?ex=66c773e6&is=66c62266&hm=0f0d38e7557414beea400a7d9c8a8e5cd5e78291258853d1ef375398d0ffa142&") // Video 3
+scrolltextMap.set(2,"Luh Tyler - Young N*");
 
-videoMap.set(3,"https://cdn.discordapp.com/attachments/1224928873230368918/1224933953614512238/jdot.mp4?ex=661f4ba4&is=660cd6a4&hm=9048c594c2f7c33a346aab889ec205c79fbdd24b8c5bdd72acfd8f2ce44e9c9c&") // Video 4
-scrolltextMap.set(3,"JDot - Tweak Shit Pt.2");
+videoMap.set(3,"https://cdn.discordapp.com/attachments/1224928873230368918/1275867712463179997/Babyfxce_E_-_PTP_Official_Music_Video.mp4?ex=66c773e7&is=66c62267&hm=2aaf1c4012dd793165f2c57f06869c5dcd5ad747cca26b3fa131e985b9c7e2de&") // Video 4
+scrolltextMap.set(3,"Babyfxce E - PTP");
 
-videoMap.set(4,"https://cdn.discordapp.com/attachments/1224928873230368918/1224933954788921475/polo.mp4?ex=661f4ba4&is=660cd6a4&hm=d2c657345e68eeb82472026c86a3efc67defc84f12dfc37ca9c3f3e92c49ea56&") // Video 5
-scrolltextMap.set(4,"Polo G - Gang With Me");
+videoMap.set(4,"https://cdn.discordapp.com/attachments/1224928873230368918/1275867715793457193/LUCKI_-_GOODFELLAS_Official_Video.mp4?ex=66c773e7&is=66c62267&hm=cd7370af514cea7ac7d9af585a21db2d758c13ed6afcc23f601e2a4529e09da3&") // Video 5
+scrolltextMap.set(4,"Lucki - GOODFELLAS");
 
 var randomNumberZeroToOne = Math.floor(Math.random()*5);
 var currentVideo = videoMap.get(randomNumberZeroToOne);
